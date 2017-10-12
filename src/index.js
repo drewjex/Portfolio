b2359ccb7874291970f2e6d838063cf45de73457
drewjex <drewjex28+1@gmail.com>
2017-10-11 19:57:53 -0600

    more setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import './style.css';
+import { App } from './components/App';
+
+ReactDOM.render(<App />, document.querySelector("#app"));
